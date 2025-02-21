83a1faa844381434aca7ac52531b5ad7cd6bb499
= <=>
2025-02-21 18:05:56 +0700

    Jobsheet 2 - Praktikum 2 (Routing)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/Welcome', function () {
+    return ('welcome');
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return ('Satriya Viar NIM = 2341760157');
+});
+
+Route::get('/user/{name}', function ($name) { 
+    return 'My name is '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+    
+Route::get('/articles/{id}', function ($id) {
+    return "Article Page with ID $id";
+});
+
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'My name is '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '. $name;
 });

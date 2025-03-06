ffbcc436c7d7e8953c7b2681fe818fb417ccddf7
= <=>
2025-03-06 11:46:47 +0700

    Practikum 2.2 Jobsheet 3

--- Jobsheet 3/database/migrations/2025_02_27_074029_create_m_user_table.php
+++ Jobsheet 3/database/migrations/2025_02_27_074029_create_m_user_table.php
@@ -14,8 +14,13 @@ return new class extends Migration
         Schema::create('m_user', function (Blueprint $table) {
             $table->id('user_id');
             $table->unsignedBigInteger('level_id')->index();
-            $table-> 
             $table->string('username', 20)->unique();
+            $table->string('nama', 100);
+            $table->string('password', 255);
+            $table->timestamps();
+
+            // Mendifinisikan Foreign Key
+            $table-> foreign('level_id')->references('level_id')->on('m_level');
         });
     }
 

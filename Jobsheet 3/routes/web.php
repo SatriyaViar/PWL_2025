5ea87337958549e06ed461185bf54ac4c30dd471
= <=>
2025-03-06 16:06:16 +0700

    Jobsheet 3 - Praktikum 5

--- Jobsheet 3/routes/web.php
+++ Jobsheet 3/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,4 @@ Route::get('/Welcome', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);

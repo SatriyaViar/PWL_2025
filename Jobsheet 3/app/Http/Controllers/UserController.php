ec6cbbba42ef3e3cca34ba82f840177c2fcebd59
= <=>
2025-03-13 08:59:40 +0700

    Jobsheet 4 - Prakctikum 2.6

--- Jobsheet 3/app/Http/Controllers/UserController.php
+++ Jobsheet 3/app/Http/Controllers/UserController.php
@@ -9,25 +9,47 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {   
     public function index(){
-        $user = UserModel::Create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('1234'),
-                'level_id' => 2
-            ],
-        );
-
-        $user-> username = 'manager12';
-
-        $user->save();
-
-        $user->wasChange();
-        $user->wasChange();
-        $user->wasChange('username');
-        $user->wasChange(['username','level_id']);
-        $user->wasChange('nama');
-        dd($user->wasChange(['nama', 'username']));
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 
+    public function tambah() {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request) {
+        UserModel::create([
+            'username' =>$request->username,
+            'nama' =>$request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
+
+        // Menampilkan form ubah user
+        public function ubah($id)
+        {
+            $user = UserModel::find($id);
+            return view('user_ubah', ['data' => $user]);
+        }
+    
+
+    function ubah_simpan($id, Request $request) {
+        $user= UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->level_id = $request->level_id;
+
+        $user->save();
+        return redirect('/user');
+    }
+
+    function hapus($id) {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+    }
 }
